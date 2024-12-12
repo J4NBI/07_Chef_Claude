f3d15343efa19a23de23ebaaf8bcaa41054dc0aa
J4N <142495328+J4NBI@users.noreply.github.com>
2024-12-12 10:46:20 +0100

    react 18 Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,37 +4,23 @@ const Form = () => {
   let [ingredients, setIngredients] = React.useState(['Milch', 'Eier']);
   let [inputValue, setInputValue] = React.useState('')
 
-  function onSubmitClick(event) {
-    event.preventDefault()
-    const newForm = new FormData(event.currentTarget);
-    const newIngredient = newForm.get("ing");
-    if (newIngredient) {
-      setIngredients(prev => {
-        return [...prev , newIngredient]});
-      setInputValue('');
-    } 
+  function handleSubmit(e){
+    e.preventDefault();
+    const event = e.currentTarget;
+    const formData = new FormData(event);
+    const newEntry = formData.get("ing");
+    setIngredients(prev => [...prev, newEntry]);
+    event.reset(); 
   }
 
-  function handleX(e) {
-    let id = e.target.id;
-    setIngredients(prev => {
-      let liste = [...prev];
-      let indexZuEntfernen = id;
-      if (indexZuEntfernen > -1 && indexZuEntfernen < liste.length) {
-        liste.splice(indexZuEntfernen, 1);
-    }
-      return liste
-      
-    })
-  }
   return (
     <>
-    <form onSubmit={onSubmitClick} className='flex items-center flex-col md:flex-row justify-center' action="">
-      <input onChange={e => setInputValue(e.target.value)} value={inputValue} name="ing" className='rounded-md border-2  md:w-[70%] md:mr-8 h-10 p-2' type="text"  id="ingredients" placeholder='e.g. oregano' />
+    <form onSubmit={handleSubmit} className='flex items-center flex-col md:flex-row justify-center' action="">
+      <input name="ing" className='rounded-md border-2  md:w-[70%] md:mr-8 h-10 p-2' type="text"  id="ingredients" placeholder='e.g. oregano' />
       <button className="mt-2 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-md text-sm h-10 px-4 flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30  mb-2">+ Add ingredients</button>
     </form>
     <ul>
-      {ingredients.map((i,index) => <li ><span>👉</span> {i} <span id={index} onClick={handleX}>X</span> </li>)}
+      {ingredients.map((i,index) => <li ><span>👉</span> {i}</li>)}
     </ul>
     </>
   )
